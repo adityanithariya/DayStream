9bbab9e463191a0bfed37df8e9487cbc419d9158
Aditya Soni <adityanithariya@gmail.com>
2024-05-01 23:41:16 +0530

    Progressive Web App Configured

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,7 +1,18 @@
+'use client'
+
 import type { NextPage } from 'next'
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const Home: NextPage = () => {
+  useEffect(() => {
+    ;(async () => {
+      const res = await fetch('http://localhost:5000/auth/protect', {
+        credentials: 'include',
+      })
+      const data = await res.json()
+      console.log(data)
+    })()
+  })
   return <div>Hello World!</div>
 }
 
