953f65e414670ca45f15e017bec5decf76cfe04a
Aditya Soni <adityanithariya@gmail.com>
2024-05-26 20:08:27 +0530

    Deployment Bugfixes

--- server/model/User.model.ts
+++ server/model/User.model.ts
@@ -1,4 +1,4 @@
-import { compare, genSalt, hash } from 'bcrypt'
+import { compare, genSalt, hash } from 'bcryptjs'
 import { type Document, Schema, model } from 'mongoose'
 
 export interface IUser extends Document {
